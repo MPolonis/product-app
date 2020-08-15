9d3386d004ba750a74596a53b86367574924ac89
Marcin Polonis <marcin.polonis@o2.pl>
2020-08-15 16:33:10 +0200

    Learn using API Redux

---
+++ productapp/src/StoreAccess.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { startCreatingProduct } from "./store/stateActions";
+
+export class StoreAccess extends Component {
+    constructor(props) {
+        super(props);
+        this.selectors = {
+            product: (storeState) => storeState.modelData.products[0],
+            state: (storeState) => storeState.stateData
+        }
+        this.state = this.selectData();
+    }
+
+    selectData() {
+        let storeState = this.props.store.getState();
+        return Object.entries(this.selectors).map(([k, v]) => [k, v(storeState)])
+            .reduce((result, [k, v]) => ({ ...result, [k]: v }), {});
+    }  
+
+    handleDataStoreChange() {
+        let newData = this.selectData();
+        Object.keys(this.selectors)
+            .filter(key => this.state[key] !== newData[key])
+            .forEach(key => this.setState({[key]: newData[key]}));
+    }
+
+    componentDidMount() {
+        this.unsubscriber = this.props.store.subscribe(() => this.handleDataStoreChange());
+    }
+    
+    componentWillUnmount() {
+        this.unsubscriber();
+    }
+
+    dispatchAction = () => {
+        this.props.store.dispatch(startCreatingProduct())
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="text-center">
+                    <button className="btn btn-primary m-1"
+                        onClick={this.dispatchAction}>
+                        Wyślij akcję
+                    </button>
+                </div>
+                <div className="bg-info">
+                    <pre className="text-white">
+                        {JSON.stringify(this.state, null, 2)}
+                    </pre>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
